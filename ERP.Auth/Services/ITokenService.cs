403da85b1450b7a4204fce3a958a9bdbf6f6f916
LaukikShelke <69532824+LaukikShelke@users.noreply.github.com>
2025-09-18 21:41:22 +0530

    Feat : Token Generation with Jwt added (#6)

---
+++ ERP.Auth/Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erp.Auth.Services
+{
+    public interface ITokenService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ ERP.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Erp.Auth.Services;
+using ERP.Business.Services.Interfaces;
+using ERP.Infrastructure.Entities;
+using ERP.WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly IUserService _userService;
+        public AuthController(ITokenService tokenService, IUserService userService)
+        {
+            _tokenService = tokenService;
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+           
+            var user = await _userService.GetUserByEmail(request.Email);
+                
+
+            if (user == null)
+                return Unauthorized("Invalid credentials");
+
+            
+            if (user.Password != request.Password)
+                return Unauthorized("Invalid credentials");
+
+            
+            var token = _tokenService.GenerateToken(user);
+            return Ok(new { Token = token });
+        }
+        [HttpGet("secure")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult SecureEndpoint()
+        {
+            return Ok("This is protected data visible only to Admins.");
+        }
+    }
+}
